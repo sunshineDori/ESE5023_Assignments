66fce163a190fda5cf82a6c1b867330760401ec2
sunshineDori <taotao.sun@foxmail.com>
2020-10-08 16:12:27 +0800

    Assignment 01-7
    Download the VQC00670480.csv from  NOAA Land-Based Datasets and Products.
    (1)Load the csv file, and clean possible data points with missing values or bad quality.
    (2)Plot the time series of the daily sum precipitation after 2016. 
    (3) Conduct at least 5 simple statistical checks(sum, mean, median, min, max) with the variable, and the result is 
    print(c(sum_cal,mean_cal,median_cal,min_cal,max_cal)) [1] 2557.00000   11.78341    0.00000    0.00000  331.00000
    print(c(min_date,max_date)) [1] "2017-01-01" "2017-03-24"

---
+++ PS1_7.R
@@ -0,0 +1,35 @@
+# Read the Precipitation csv file 
+Precipitation_Data <- read.csv(file = "VQC00670480.csv", header = T) 
+names(Precipitation_Data) 
+Pre_V<-Precipitation_Data$DlySum 
+head(Pre_V) 
+tail(Pre_V) 
+typeof(Pre_V) 
+
+Obs_Time<-Precipitation_Data$DATE 
+head(Obs_Time) 
+tail(Obs_Time) 
+typeof(Obs_Time) 
+
+#Clean the data, make the missing data NA; retain the data after 2016 to plot 
+Pre_Vnum <- as.numeric(Pre_V) 
+Pre_V_flag <- Precipitation_Data$DlySumQF 
+Pre_Vnum[which(Pre_V_flag == "M")]  <- NA 
+Pre_V_cal <- Pre_Vnum[which(Obs_Time > "2016-12-31")] 
+Obs_Time2  <- Obs_Time[which(Obs_Time > "2016-12-31")] 
+Obs_Time_cal <- as.Date(Obs_Time2) 
+
+#Plot the time series of the daily sum precipitation after 2016. 
+plot(Obs_Time_cal,Pre_V_cal, lwd=0.5,type="p",col="blue") 
+
+#statistical checks with the daily sum precipitation after 2016. 
+sum_cal <- sum(Pre_V_cal,na.rm=T) 
+mean_cal <- mean(Pre_V_cal,na.rm=T) 
+median_cal <- median(Pre_V_cal,na.rm=T) 
+min_cal <- min(Pre_V_cal,na.rm=T) 
+max_cal <- max(Pre_V_cal,na.rm=T) 
+min_date <- Obs_Time_cal[which.min(Pre_V_cal)] 
+max_date <- Obs_Time_cal[which.max(Pre_V_cal)] 
+
+print(c(sum_cal,mean_cal,median_cal,min_cal,max_cal)) 
+print(c(min_date,max_date)) 
